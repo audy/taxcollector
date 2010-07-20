1a43c6f4efda93f3e646efbb50e9f382b4c0b1e5
Austin G. Davis-Richardson <harekrishna@gmail.com>
2010-07-20 17:48:37 -0400

    minor refactorage

--- rdp.py
+++ rdp.py
@@ -16,9 +16,7 @@
 			i.e. [0]Bacteria;[1]Cyanobacteria;[2]"Cyanobacteria";[3]Oscillatoriales;[4]"Oscillatoriales";[5]Geitlerinema;[6]Geitlerinema sp. SP19606-11
 '''
 
-import os
 import sys
-import string
 
 # Edit these as you wish. (case-sensitive!):
 # taxExemption - levels of taxonomy to exclude.
@@ -26,113 +24,86 @@ taxExemption = ['subspecies','subgenus','subfamily','suborder','subclass','subph
 # concerned - levels of taxonomy to include in the final output (IN ORDER).
 taxInclusion =  [ 'superkingdom','phylum','class','order','family','genus','species' ]
 
-
 def main(argv):
-	
+	''' The Fish & Chips '''
 	if not len(argv) == 5:
 		print 'USAGE:', argv[0], '<names file> <nodes file> <RDP file> <output file>'
 		quit(2)
+	fnNames, fnNodes, fnRDP, fnOutput = argv[1:5]
 	
-	fnNames,fnNodes,fnRDP,fnOutput = argv[1:5]
+	print 'Loading NCBI Names: %s & Nodes: %s' % (fnNames, fnNodes)
+	Names, rNames, Nodes = loadTax(fnNames, fnNodes)
 	
-	print 'Loading NCBI databases:', fnNames, fnNodes
-	
-	Names,rNames,Nodes = loadTax(fnNames, fnNodes)
-	
-	print 'Loading RDP database:', fnRDP
-	#fastaDB = loadGreenGenes(fnRDP)
+	print 'Loading RDP database: %s' % fnRDP
 	fastaDB = loadRDP(fnRDP)
 		
 	print 'Searching...'
-	
 	for item in fastaDB:
 		nom = item[0].replace(' ','')
 		if nom in Names:
 			item.append( getNames( getIDs(nom, Names, rNames, Nodes), rNames) )
-		
+	
+	print 'Saving...'
 	saveDB(fastaDB,fnOutput)
 	
 
-def saveDB(RDP,fnOutput):
-	
+def saveDB(RDP, fnOutput):
+	''' Saves TaxCollected Database'''
 	try: 
 		output = open(fnOutput,'w')
 	except: 
 		print 'ERROR: unable to open file for output.'
 		quit(2)
-		
-	print 'Saving:',fnOutput
-	
+	print 'Saving: %s' % fnOutput
 	counter = 0
-	
 	for item in RDP:
 		try: 
 			output.write( '>' + item[2].replace(' ','_') + '\n' + item[1] )
 			counter += 1
 		except: pass
-			
-	print 'Saved', counter, 'records.'
+	print 'Saved %s records.' % counter
 		
 
 def getNames(TaxIDs, rNames):
-	
-	taxdirt = {}
-	taxlist = []
-	taxonomy = ''
-	
+	''' gets Names from a list of TaxIDs '''
+	taxdirt, taxlist, taxonomy = {}, [], ''
 	for i in range(len(TaxIDs)):
 		taxid, level = TaxIDs[i]
 		name = rNames[taxid]
-		
 		if level not in taxExemption:
 			if level in taxInclusion:
 				taxdirt[level] =  name
-				
 	for i in range(len(taxInclusion)):
 		level = taxInclusion[i]
 		if level in taxdirt:
 			taxlist.append( [ i,  taxdirt[level] ])
-			
 	next = [0, '']
-	
 	taxlist.reverse()
-	
 	for item in taxlist:
 		if next[0] - item[0] == 2:
 			taxonomy = '[' + str(next[0]-1) + ']\"' + next[1] + '\";'+ taxonomy
 		next = item
 		taxonomy = '[' + str(item[0]) + ']' + item[1] + ';' + taxonomy
-				
 	return taxonomy.strip(';')
 
 
 def getIDs(name, Names, rNames, Nodes):
-	
-	idlist = []
-	newid = []
-	
-#	for item in name:
-#		if item in Names:
-#			newid = [ Names[item], 'species' ] 
-#			break
-	
+	''' Gets IDs given a name '''
+	idlist, newid = [], []
 	if name in Names:
 		newid = [ Names[name], 'species' ] 
 	else:
 		return 0
-	
 	idlist.append(newid)
-	
 	while not '1' == newid[0]:
 		if newid[0] in Nodes:
 			newid = Nodes[newid[0]]
 			idlist.append([newid[0],Nodes[newid[0]][1]])
-			
 	return idlist
 
 	
 def loadGreenGenes(fnGG):
-	
+	''' loads GreenGenes database. '''
 	try:
 		GG = open(fnGG,'r')
 	except:
@@ -159,17 +130,14 @@ def loadGreenGenes(fnGG):
 			
 
 def loadRDP(fnRDP):
-	
+	''' loads RDP database. '''
 	try:
 		fasta = open(fnRDP,'r')
 	except:
 		print 'ERROR: unable to open RDP file for reading.'
 		quit(2)
-		
 	RDP = []
-	
 	counter, sequence, header, Database = 0, '', '', []
-	
 	for line in fasta:
 		if line[:1] == '>':
 			counter += 1
@@ -178,23 +146,20 @@ def loadRDP(fnRDP):
 			header = line[line.find(' ')+1:line.find(';')].replace('(T)', '').strip()
 		else:
 			sequence += line
-			
 	fasta.close()
-	
-	print 'loaded', counter, 'RDP records.'
+	print 'loaded %s RDP records.' % counter
 	return RDP
 	
 
 def loadTax(fnNames='names.dmp', fnNodes='nodes.dmp'):
-	
+	'''  loads Taxonomy Databases '''
 	counter, rNames, Names, Nodes = 0, {}, {}, {}
-	
 	try:
 		namesfile, nodesfile = open(fnNames, 'r'), open(fnNodes, 'r')
 	except:
 		print 'ERROR: unable to open Names and/or Nodes file for reading.'
 		quit(2)
-		
+	
 	for line in namesfile:
 		line = line.split('\t|\t')
 		taxid, name, nametype = line[0], line[1], line[3].replace('\t|\n', '')
@@ -202,18 +167,14 @@ def loadTax(fnNames='names.dmp', fnNodes='nodes.dmp'):
 		if 'scientific name' == nametype:
 			Names[name.replace(' ', '')] = taxid
 			rNames[taxid] = name
-			
-	print 'loaded', counter, 'names from', fnNames
+	print 'loaded %s names from %s' % (counter, fnNames)
 	
 	counter = 0
-	
 	for line in nodesfile:
 		counter += 1
 		taxid, parentid, childtype = line.split('\t|\t')[:3]
 		Nodes[taxid] = [parentid, childtype]
-		
-	print 'loaded', counter, 'nodes from', fnNodes
-	
+	print 'loaded %s nodes from %s' %  (counter, fnNodes)
 	nodesfile.close()
 	namesfile.close()
 	
@@ -221,4 +182,8 @@ def loadTax(fnNames='names.dmp', fnNodes='nodes.dmp'):
 
 	
 if __name__ == '__main__':
-	main(sys.argv)
+	try:
+	    main(sys.argv)
+	except KeyboardInterrupt:
+	    print >> sys.stderr, 'ouch!\n'
+	    quit()
