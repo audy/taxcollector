8a4fdaf7c51858d608b9bdcf7323a5d8007d309e
Austin G. Davis-Richardson <harekrishna@gmail.com>
2010-07-21 12:54:20 -0400

    refactorage

--- acctotax.py
+++ acctotax.py
@@ -4,19 +4,17 @@
 Acc to Tax:
 
 Generates a flat-file database for use in green.py
+Database contains ProkMSA IDs from the GreenGenes Database &
+their corresponding NCBI taxids.
 
-Database contains ProkMSA IDs from the GreenGenes Database & their corresponding NCBI taxids.
+usage: acctotax.py inputfile
 """
 
 import sys
-import os
-import string
-
 
 def main(argv):
-
+    '''  '''
 	taxi, prokMSA = '', ''
-
 	try:
 		handle = open(argv[1],'r')
 		outtie = open(argv[1] + 'out','w')
@@ -24,15 +22,12 @@ def main(argv):
 		print 'USAGE:', argv[0], 'inputfile'
 		
 	print 'Generating proktable from', argv[1]
-		
 	for line in handle:
 		if line[:12] == 'ncbi_tax_id=':
 			taxi = line[12:]
 		if line[:11] == 'prokMSA_id=':
 			prokMSA = line[11:].strip()
 			outtie.write(prokMSA + '\t|\t' + taxi)
-
-		
 	print 'Done generating.'
 	print 'Proktable saved as:', argv[1] + 'out'		
 	
