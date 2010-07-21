b2817c4320b0c2ecbce19e48c7a07d976a5b0de9
Austin G. Davis-Richardson <harekrishna@gmail.com>
2010-07-21 13:24:03 -0400

    you need one of these if you are to use git

--- common.py
+++ common.py
@@ -7,17 +7,16 @@
 # University of Florida
 # Gainesville, FL
 
-import sys
-
 # Edit these as you wish. (case-sensitive!):
 # taxExemption - levels of taxonomy to exclude.
+
 taxExemption = ['subspecies','subgenus','subfamily','suborder','subclass',
 'subphylum','subkingdom','superspecies','superfamily','superorder',
 'superclass','superphylum','no rank','tribe','authority','varietas',
 'species group','species subgroup','infaclass']
 
 # concerned - levels of taxonomy to include in the final output (IN ORDER).
-taxInclusion =  [ 'superkingdom','phylum','class',
+taxInclusion =	[ 'superkingdom','phylum','class',
 'order','family','genus','species' ]
 
 def main_rdp(argv):
@@ -32,7 +31,7 @@ def main_rdp(argv):
 	
 	print 'Loading RDP database: %s' % fnRDP
 	fastaDB = loadRDP(fnRDP)
-		
+	
 	print 'Searching...'
 	for item in fastaDB:
 		nom = item[0].replace(' ','')
@@ -41,50 +40,50 @@ def main_rdp(argv):
 	
 	print 'Saving...'
 	saveDB(fastaDB,fnOutput)
-	
+
 
 def main_gg(argv):
-    ''' main() for GreenGenes '''
+	''' main() for GreenGenes '''
 	if not len(argv) == 6:
 		print 'USAGE:',argv[0],'<names file> <nodes file> <ProkDB file> <RDP file> <output file>'
 		quit(2)
-
+	
 	fnNames, fnNodes, fnProk, fnDB, fnOutput = argv[1:6]
-
+	
 	print 'Loading NCBI Names: %s & Nodes: %s' % (fnNames, fnNodes)
 	rNames, Nodes = loadTax(fnNames,fnNodes)
-
+	
 	print 'Loading GreenGenes database: %s' % fnDB
 	fastaDB = loadGreenGenes(fnDB)
 	
-	print 'Loading ProkMSA database: %s', % fnProk
+	print 'Loading ProkMSA database: %s' % fnProk
 	ProkIDs = loadProkIds(fnProk)
-
+	
 	print 'Searching...'
 	for item in fastaDB:
 		prokID = item[0]
 		if prokID in ProkIDs:
 			item.append( getNames( getIDs(prokID,ProkIDs,Nodes),rNames) )
-
+	
 	saveDB(fastaDB,fnOutput)
 
 
 def saveDB(RDP, fnOutput):
 	''' Saves TaxCollected Database'''
-	try: 
+	try:
 		output = open(fnOutput,'w')
-	except: 
+	except:
 		print 'ERROR: unable to open file for output.'
 		quit(2)
 	print 'Saving: %s' % fnOutput
 	counter = 0
 	for item in RDP:
-		try: 
+		try:
 			output.write( '>' + item[2].replace(' ','_') + '\n' + item[1] )
 			counter += 1
 		except: pass
 	print 'Saved %s records.' % counter
-		
+
 
 def getNames(TaxIDs, rNames):
 	''' gets Names from a list of TaxIDs '''
@@ -113,7 +112,7 @@ def getIDs(name, Names, rNames, Nodes):
 	''' Gets IDs given a name '''
 	idlist, newid = [], []
 	if name in Names:
-		newid = [ Names[name], 'species' ] 
+		newid = [ Names[name], 'species' ]
 	else:
 		return 0
 	idlist.append(newid)
@@ -122,16 +121,16 @@ def getIDs(name, Names, rNames, Nodes):
 			newid = Nodes[newid[0]]
 			idlist.append([newid[0],Nodes[newid[0]][1]])
 	return idlist
-
 	
+
 def loadGreenGenes(fnGG):
-    ''' loads GreenGenes database. '''
+	''' loads GreenGenes database. '''
 	try:
 		GG = open(fnGG,'r')
 	except:
 		print 'ERROR: unable to open GreenGenes file for reading.'
 		quit(2)
-
+	
 	Genes = []
 	counter,sequence,prokid,Database = 0,'','',[]
 	for line in GG:
@@ -144,8 +143,8 @@ def loadGreenGenes(fnGG):
 			except: pass
 		else:
 			sequence += line
-	print ' %s GreenGenes records loaded from %s' %  (counter, fnGG)
-
+	print ' %s GreenGenes records loaded from %s' %	 (counter, fnGG)
+	
 	return Genes
 
 
@@ -169,10 +168,10 @@ def loadRDP(fnRDP):
 	fasta.close()
 	print 'loaded %s RDP records.' % counter
 	return RDP
-	
+
 
 def loadTax(fnNames='names.dmp', fnNodes='nodes.dmp'):
-	'''  loads Taxonomy Databases '''
+	'''	 loads Taxonomy Databases '''
 	counter, rNames, Names, Nodes = 0, {}, {}, {}
 	try:
 		namesfile, nodesfile = open(fnNames, 'r'), open(fnNodes, 'r')
