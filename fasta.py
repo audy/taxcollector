6152d4b062533c0da1fa9a1719498d86ffbe3833
Austin G. Davis-Richardson <harekrishna@gmail.com>
2010-07-21 13:10:05 -0400

    not supposed to be here yet

--- fasta.py
+++
@@ -1,9 +0,0 @@
-class Fasta:
-    ''' Fasta file iterator '''
-    def __init__(self):
-        pass
-        
-class Dna:
-    ''' Object representing a FASTA record '''
-    def __init__(self):
-        pass
